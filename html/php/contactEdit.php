db175ee0660f83787a6c9f9ac5d99998e282022d
root <13dharmeyer@gmail.com>
2019-01-24 17:25:14 +0000

    added contactEdit, tested with ARC

---
+++ html/php/contactEdit.php
@@ -0,0 +1,57 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$passwordLocation="/home/david/SQLPassword.txt";
+        $passwordFile=fopen($passwordLocation, "r") or die("unable to open file!");
+        $sqlpassword=trim(fread($passwordFile, filesize($passwordLocation)));
+        fclose($passwordFile);
+
+	//$sql="SELECT * FROM users WHERE username='".$usernameGiven."'";
+	$sql="UPDATE contacts set first_name=?, last_name=?, phone=?, email=?, address=?, birthdate=?, corgo_pic_url=? ".
+		"WHERE contact_id=?;";
+	$conn= new mysqli("localhost", "root", $sqlpassword, "poosdsmall");
+
+	if ($conn->connect_error) {
+		returnWithError($conn->connect_error);
+		return;
+	}
+
+	$prepared=$conn->prepare($sql);
+	$prepared->bind_param("ssssssss", $firstName, $lastName, $phone, $email, $address, $birthdate, $corgoPicUrl, $contactId);
+
+	$firstName=$inData["first_name"];
+	$lastName=$inData["last_name"];
+	$phone=$inData["phone"];
+	$email=$inData["email"];
+	$address=$inData["address"];
+	$birthdate=$inData["birthdate"];
+	$corgoPicUrl=$inData["corgo_pic_url"];
+	$contactId=$inData["contact_id"];
+
+	$prepared->execute();
+	$conn->close();
+
+
+	function getRequestInfo() {
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $error ) {
+		$retValue='{"user_id":0, "username":"", "password_hash":"", "Error": "'.$error.'"}';
+		return sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithInfo( $id, $username, $password_hash )
+	{
+ 		$retValue = '{"user_id":' . $id . ',"username":"' . $username . '","password_hash":"' . $password_hash . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
